df4a766cfd0dd7a2b89cb7f3db6cbfee02e9c5b1
Januarius-Njoku <76001800+Januarius-Njoku@users.noreply.github.com>
2022-04-28 01:52:51 +0100

    Added assignments 15 andf 16 to the repo

---
+++ Assignment15/Assignment15/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Assignment15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IndexOutOfRangeHandler();
+            Console.WriteLine();
+            OverflowExceptionHandler();
+            Console.WriteLine();
+            NullReferenceExceptionHandler();
+            Console.WriteLine();
+            InvalidCastExceptionHandler();
+            Console.WriteLine();
+            DirectoryNotFoundExceptionHandler();
+            Console.WriteLine();
+            FileNotFoundExceptionHandler();
+            Console.WriteLine();
+            FormatExceptionHandler();
+
+
+            Console.WriteLine();
+            // ProductNotFound test
+            List<string> products = new List<string>() { "apple", "orange" };
+            try
+            {
+                if (products.Contains("banana"))
+                {
+                    Console.WriteLine("banana has been added to your cart");
+                }
+                else
+                {
+                    throw (new ProductNotFoundException("The product is out of stock."));
+                }
+            }
+            catch (ProductNotFoundException pnfe)
+            {
+                Console.WriteLine(pnfe.Message);
+            }
+
+
+
+            Console.WriteLine();
+            // LessThanException test
+
+            int a = 7;
+            int b = 5;
+
+            try
+            {
+                if (b < 7)
+                {
+                    throw (new LessThanException("The number is less than the other."));
+                }
+                
+            }
+            catch(LessThanException lte)
+            {
+                Console.WriteLine(lte.Message);
+                Console.WriteLine("You need a larger Number'.");
+            }
+
+
+
+            Console.WriteLine();
+            // InputNotValidExceptionTest
+            string input = "rtthj6h54urnggf";
+            try
+            {
+                if(input == "password123")
+                {
+                    Console.WriteLine("Access Granted");
+                }
+                else
+                {
+                    throw (new PasswordNotValidException("Incorrect Password."));
+                }
+            }
+            catch(PasswordNotValidException pnve)
+            {
+                Console.Write("An error occured: ");
+                Console.WriteLine(pnve.Message);
+            }
+
+        }
+
+
+
+
+
+
+
+        public static void IndexOutOfRangeHandler()
+        {
+            int[] intArray = new int[5] { 3, 4, 5, 6, 7 };
+
+            try
+            {
+                Console.WriteLine(intArray[8]);
+            }
+            catch(IndexOutOfRangeException iore)
+            {
+                Console.WriteLine("The index specified is not valid.");
+                Console.WriteLine(iore.Message);
+            }
+        }
+
+
+        public static void OverflowExceptionHandler()
+        {
+            double myDouble = 4445433345433455;
+            checked
+            {
+                try
+                {
+                    sbyte mySByte = (sbyte)myDouble;
+                    Console.WriteLine(mySByte);
+                }
+                catch (OverflowException oe)
+                {
+                    Console.WriteLine(oe.Message);
+                    Console.WriteLine(myDouble + ">" + SByte.MaxValue);
+                }
+            }
+        }
+
+        public static void NullReferenceExceptionHandler()
+        {
+            string myText = null;
+            try
+            {
+                Console.WriteLine(myText.Length);
+            }
+            catch(NullReferenceException nre)
+            {
+                Console.WriteLine(nre.Message);
+                Console.WriteLine("null operation is invalid");
+            }
+        }
+
+        public static void DirectoryNotFoundExceptionHandler()
+        {
+            string fileLocation = @"C://truth";
+
+            try
+            {
+                Directory.SetCurrentDirectory(fileLocation);
+            }
+            catch(DirectoryNotFoundException dnfe)
+            {
+                Console.WriteLine("Directory not found:");
+                Console.WriteLine(dnfe.Message);
+            }
+
+            
+
+        }
+
+        public static void InvalidCastExceptionHandler()
+        {
+            object anObject = "10";
+            try
+            {
+                int x = (int)anObject;
+            }
+            catch(InvalidCastException ice)
+            {
+                Console.WriteLine(ice.Message);
+            }
+        }
+
+        public static void FileNotFoundExceptionHandler()
+        {
+            string fileName = "santisimo.pfd";
+            try
+            {
+                TextReader reader = new StreamReader(fileName);
+                string newLine = reader.ReadLine();
+                reader.Close();
+            }
+            catch(FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+           
+        }
+
+        public static void FormatExceptionHandler()
+        {
+            decimal amount = 3445.664m;
+            try
+            {
+                Console.WriteLine("The amount given is {0:Q44}", amount);
+            }
+            catch(FormatException fe)
+            {
+                Console.Write("An exception occured: ");
+                Console.WriteLine(fe.Message);
+            }
+        }
+       
+
+    }
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+
+    public class LessThanException: Exception
+    {
+        public LessThanException(string message): base(message)
+        {
+
+        }
+    }
+
+
+    public class PasswordNotValidException: Exception
+    {
+        public PasswordNotValidException(string message): base(message)
+        {
+
+        }
+    }
+
+}
---
+++ Assignment16/Assignment16/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Assignment16
+{
+    internal class Program
+    {
+        public static string text = "StarLight is the light emitted by start.[1] It typically reders to visible electromagnetic radiation from stars" +
+                            "other than the Sun, observable from the Earth at night, althoug a component of starlight is observable from Earth during daytime. " +
+                            "Sunlight is the term used for the Sun's starlgiht observed during daytime.[2] " +
+                            "During nighttime, albedo describes solar reflections from other Solar System objects, including moonlight, planet shine and zodiacal light. [3]";
+
+        static void Main(string[] args)
+        {
+            StringMethod();
+        }
+
+        public static void StringMethod()
+        {
+            // 1. Extracting parts of the text
+            int index1 = text.IndexOf("observable");
+            string firstPart = text.Substring(index1, 15);
+            int index2 = text.IndexOf("zodiacal");
+            string secondPart = text.Substring(index2 - 10, 10);
+
+            string fullText = firstPart + secondPart;
+            Console.WriteLine(fullText);
+
+
+            // 2. Replacing Sun in the text to "Solar Emission".
+            string newText = text.Replace("Sun", "Solar Emission");
+            Console.WriteLine(newText);
+
+
+            // Writing the index of all occurences of "Solar Emission" to the console
+            int sunIndex = newText.IndexOf("Solar Emission");
+
+            while (sunIndex != -1)
+            {
+                Console.WriteLine(sunIndex);
+                sunIndex = newText.IndexOf("Solar Emission", sunIndex + 1);
+            }
+
+            // 3. Splitting the text based on each sentences
+
+            char[] separators = new char[] { '.' };
+            string[] splitText = text.Split(separators);
+
+            string mergedString = splitText[0] + '.' + splitText[splitText.Length - 2] + '.';
+            Console.WriteLine(mergedString);
+
+            // 4. Extracting all the reference numbers
+
+
+            Regex rx = new Regex("[0-9]");
+            MatchCollection matches = rx.Matches(text);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
+            // 5a. List of all Vowel Characters
+            Console.WriteLine();
+            Console.WriteLine("The vowel charaters are: ");
+            List<string> vowelCharacters = new List<string>();
+
+            Regex vCharRegex = new Regex("[aeiou]");
+            MatchCollection vowelMatches = vCharRegex.Matches(text);
+            foreach (Match match in vowelMatches)
+            {
+                vowelCharacters.Add(match.Groups[0].Value);
+            }
+
+            foreach(string item in vowelCharacters)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // 5b. List of all Consonant Characters
+            Console.WriteLine();
+            Console.WriteLine("The consonant characters are: ");
+            List<string> consonantCharacters = new List<string>();
+
+            Regex cCharRegex = new Regex("[qwrtyupsdfghjklzxcvbnm]");
+            MatchCollection consonantMatches = cCharRegex.Matches(text);
+            foreach (Match match in consonantMatches)
+            {
+                consonantCharacters.Add(match.Groups[0].Value);
+            }
+
+            foreach (string item in consonantCharacters)
+            {
+                Console.WriteLine(item);
+            }
+            //5c. List of all Number Characters
+            Console.WriteLine();
+            Console.WriteLine("The numbers in the text are: ");
+            List<string> numbers = new List<string>();
+
+            Regex numRegex = new Regex("[0-9]");
+            MatchCollection numMatches = numRegex.Matches(text);
+            foreach (Match match in numMatches)
+            {
+               numbers.Add(match.Groups[0].Value);
+            }
+
+            foreach (string item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            // 5d. List of all Special Characters
+            Console.WriteLine();
+            Console.WriteLine("The Special Characters in the text are: ");
+            List<string> specialCharacters = new List<string>();
+
+            Regex sCharsRegex = new Regex("[\\[\\]\\.,!@#$%^&*|]");
+            MatchCollection sCharMatches =sCharsRegex.Matches(text);
+            foreach (Match match in sCharMatches)
+            {
+                specialCharacters.Add(match.Groups[0].Value);
+            }
+
+            foreach (string item in specialCharacters)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
